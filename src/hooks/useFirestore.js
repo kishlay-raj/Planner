5ae8f14f8dda9be80792c48aa3d4f75a6f55d108
kishlay-raj <150506011+kishlay-raj@users.noreply.github.com>
2026-06-07 19:14:34 +0530

    Update useFirestore.js
    added callback

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { doc, onSnapshot, setDoc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
